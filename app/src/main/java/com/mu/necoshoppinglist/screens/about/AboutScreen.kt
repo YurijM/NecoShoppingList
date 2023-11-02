81d9703c52523f44adc3a3200ec8e0d608e7da00
YurijM <y-myagkov@mail.ru>
2023-11-02 11:24:32 +0300

    Создание разметки страницы "About" (урок 41)

--- app/src/main/java/com/mu/necoshoppinglist/screens/about/AboutScreen.kt
+++ app/src/main/java/com/mu/necoshoppinglist/screens/about/AboutScreen.kt
@@ -1,22 +1,62 @@
 package com.mu.necoshoppinglist.screens.about
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mu.necoshoppinglist.R
+import com.mu.necoshoppinglist.ui.theme.BlueLight
+import com.mu.necoshoppinglist.ui.theme.BlueMain
 
 @Composable
 fun AboutScreen(
     paddingValues: PaddingValues
 ) {
-    Text(
+    val uriHandler = LocalUriHandler.current
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .wrapContentWidth()
-            .wrapContentHeight(),
-        text = "About Screen"
-    )
+            .padding(bottom = paddingValues.calculateBottomPadding())
+            .background(BlueLight),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            modifier = Modifier.size(150.dp),
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo",
+            tint = BlueMain
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = "Разработано MU & C°",
+            fontSize = 20.sp,
+            color = BlueMain
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+            modifier = Modifier.clickable {
+                uriHandler.openUri("http://skf-bgtu.ru")
+            },
+            text = ">>> Подробнее <<<",
+            color = Color.Blue
+        )
+    }
 }
